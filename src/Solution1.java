d9bc9b47783ab4491709c6690e1f04d1ae667e84
Merve Alpay <mervealpay64@gmail.com>
2024-07-08 14:38:24 +0300

    Update Solution1.java

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,4 +1,9 @@
-import org.opencv.core.*;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.List;
@@ -15,43 +20,43 @@ public class Solution1 extends ImageProcessor {
         Mat gray = new Mat();
         Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
 
-
+        // Kontrastı artırmak için histogram dengeleme
         Mat equalized = new Mat();
         Imgproc.equalizeHist(gray, equalized);
 
-     
+        // Adaptif eşikleme (Daha geniş eşikleme penceresi)
         Mat thresholded = new Mat();
         Imgproc.adaptiveThreshold(equalized, thresholded, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 15, 10);
 
-      
+        // Morfolojik işlemler (Daha küçük kernel boyutu)
         Mat morphKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
         Mat morphed = new Mat();
         Imgproc.morphologyEx(thresholded, morphed, Imgproc.MORPH_CLOSE, morphKernel);
 
-   
+        // Kenar algılama
         Mat edges = new Mat();
         Imgproc.Canny(morphed, edges, 50, 150);
 
-  
+        // Konturları bul
         List<MatOfPoint> contours = new java.util.ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-
+        // Konturları dikdörtgenlerle çerçevele
         Mat output = src.clone();
         for (MatOfPoint contour : contours) {
             Rect rect = Imgproc.boundingRect(contour);
 
-         
+            // Minimum boyut kontrolü
             if (rect.width < 20 || rect.height < 20) {
-                continue;
+                continue; // Küçük konturları atla
             }
 
-         
+            // Renk ortalamasını hesapla
             Scalar meanColorRect = Core.mean(new Mat(src, rect));
             double[] meanRectColor = {meanColorRect.val[2], meanColorRect.val[1], meanColorRect.val[0]};
 
-        
+            // Renk ortalamasını ve toplam görüntü ortalamasını kullanarak kontrast hesaplama
             double[] meanImageColor = Core.mean(src).val;
             double contrast = ContrastUtils.calculateContrast(meanRectColor, meanImageColor);
 
