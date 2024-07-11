4d41f3e048c71e3f452ac139518c85f9d52bfd09
alpayyy <115874416+alpayyy@users.noreply.github.com>
2024-07-11 09:43:01 +0300

    Revert "Yaptığınız değişiklikleri açıklayan mesaj"
    This reverts commit 5bb428d9ff1735a38ee806988fdcad1e7b580243.

--- src/com/ias/test/imageprocess/Main.java
+++ src/com/ias/test/imageprocess/Main.java
@@ -1,9 +1,14 @@
 package com.ias.test.imageprocess;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+
+import com.ias.test.imageprocess.ImageProcessor;
 import com.ias.test.imageprocess.azat.Solution2;
-import com.ias.test.imageprocess.morphology.Morphology;
-import com.ias.test.imageprocess.morphology.contrast.ContrastCalculator;
-import com.ias.test.imageprocess.morphology.contrast.ContrastUtilsImp;
+import com.ias.test.imageprocess.morphology.Solution1;
 
 import java.util.Scanner;
 
@@ -13,21 +18,25 @@ public class Main {
         System.out.println("OpenCV kütüphanesi yüklendi.");
     }
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Lütfen resim dosyasının yolunu giriniz:");
         String imagePath = scanner.nextLine();
 
-        System.out.println("Lütfen çözüm yolunu seçiniz (1:Merve veya 2):");
+        Mat src = Imgcodecs.imread(imagePath);
+        if (src.empty()) {
+            System.out.println("Görsel yüklenemedi: " + imagePath);
+            return;
+        }
+
+        System.out.println("Lütfen çözüm yolunu seçiniz (1 veya 2):");
         int choice = scanner.nextInt();
-        double contrastThreshold = 1.5;
+        double contrastThreshold =1.5;
         ImageProcessor processor;
-        ContrastCalculator contrastCalculator = new ContrastUtilsImp();
 
         if (choice == 1) {
-            processor = new Morphology(contrastThreshold,contrastCalculator);
+            processor = new Solution1(contrastThreshold);
         } else if (choice == 2) {
             processor = new Solution2(contrastThreshold);
         } else {
@@ -35,7 +44,17 @@ public class Main {
             return;
         }
 
-      
-        processor.process(imagePath);
+        Mat output = processor.processImage(src);
+
+        Mat resizedOutput = new Mat();
+        Size newSize = new Size(800, 600);
+        Imgproc.resize(output, resizedOutput, newSize);
+
+        HighGui.imshow("Yazı ve Arka Plan Renkleri Çok Yakın Alanlar", resizedOutput);
+        HighGui.waitKey();
+
+        src.release();
+        output.release();
+        resizedOutput.release();
     }
 }
